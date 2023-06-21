4b2038c8b60117382300eb7ff65284c4888bc1df
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-06-21 11:23:42 +0600

    create users api

--- index.js
+++ index.js
@@ -36,8 +36,18 @@ async function run() {
 
 
         // users related apis
+        app.get('/users', async(res, req) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+
         app.post('/users', async(req, res) => {
             const user = req.body;
+            const query = {email: user.email};
+            const existingUser = await usersCollection.findOne(query);
+            if(existingUser){
+                return res.send({message: 'User already exists!'});
+            }
             const result = await usersCollection.insertOne(user);
             res.send(result);
         })
